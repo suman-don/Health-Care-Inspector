640249ae4ba60fab019d6f85199153997d315aa2
suman <suman123@gmail.com>
2025-09-28 06:15:21 +0545

    updated collectdata file

--- collectdata.cpp
+++ collectdata.cpp
@@ -1,42 +1,33 @@
 #include "cls.h"
 
 void detail::getdata() {
-    cout << "\n\n" << setw(82) << "PERSONAL DETAIL AND ALSO HEALTH ISSUE" << endl;
+       cout << "\n\n" << setw(82) << "PERSONAL DETAIL AND ALSO HEALTH ISSUE" << endl;
     cout << "\n                              YOU MUST FILL ALL INFORMATION BECAUSE THIS IS OUR FORMALITY...." << endl;
     
-    
-//    	cin.ignore();	
-	
-    
+        
     cout<< "\n                                  "<<"FULLNAME: ";
     getline(cin, name);
-
-    cout << "\n" << setw(39) << "AGE: ";
+    cout<<"\n" << setw(39) << "AGE: ";
     cin >> age;
     cin.ignore();
-
-    cout << "\n" << setw(42) << "GENDER: ";
+    cout<<"\n                                  GENDER(F/M): ";
     cin >> gender;
     cin.ignore();
-    
-    cout<< "\n                                  "<<"ADDRESS: ";
+    cout<<"\n                                  "<<"ADDRESS: ";
     getline(cin,address);
-    
     cout<<"\n                                  "<<"CONTACT NUMBER: ";
     getline(cin,contact);
-    
-    cout<<"\n                                  "<<"DATE: ";
+    cout<<"\n                                  "<<"DATE(YY/MM/DD): ";
     getline(cin,date);
-
-    cout << "\n" << setw(44) << "SYMPTOMS: ";
+    cout<<"\n                                  SYMPTOMS: ";
     getline(cin, symptom);
-
-    cout << "\n" << setw(56) << "DURATION OF SYMPTOMS: ";
+    cout<<"\n                                  DURATION OF SYMPTOMS: ";
     cin >> duration;
     cin.ignore();
-
-    cout << "\n" << setw(68) << "SEVERITY(Minor,Moderate,Serious): ";
+    cout<<"\n                                  SEVERITY(Minor,Moderate,Serious): ";
     getline(cin, severity);
+ 
+ 
     
     
 
@@ -48,10 +39,9 @@ void detail::getdata() {
 //    }
 }
 
-
-
 void detail::detecting(){
 	cout<<"\n\n\n\n\n\n\n\n\n\n\n\n                                                   DETECTING........";
     Sleep(1000);
     system("cls");
 }
+
